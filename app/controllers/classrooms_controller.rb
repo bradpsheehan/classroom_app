fa1430f97fdedf573fd5b8ca60494202e5be31c0
erindrummond <e.b.drummond@gmail.com>
2013-06-14 18:17:12 -0600

    changed unit controller tests to integration tests

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -2,18 +2,26 @@ class ClassroomsController < ApplicationController
   respond_to :json
 
   def show
-    @classroom = Classroom.find params[:id]
+    @classroom = Classroom.find(params[:id])
     respond_with @classroom
   end
 
   def create
-    @classroom = Classroom.create(params[:classroom])
-    respond_with @classroom
+    @classroom = Classroom.new(params[:classroom])
+    @classroom.save
+    if @classroom.save
+      respond_with(@classroom, :location => classroom_path(@classroom.teacher_id, @classroom))
+    else
+      respond_with(@classroom)
+    end
+
   end
 
   def update
     @classroom = Classroom.find(params[:id])
     @classroom.update_attributes(params[:classroom])
-    respond_with @classroom
+    respond_to do |format|
+      format.json { render :json => @classroom }
+    end
   end
 end
--- spec/controllers/classrooms_controller_spec.rb
+++ spec/controllers/classrooms_controller_spec.rb
@@ -4,25 +4,32 @@ describe ClassroomsController do
 
   describe "POST #create" do
 
+#TODO CHANGE STUBS TO ACTUALLY HIT THE DB
     context "with valid attributes" do
       it "saves the new classroom in the database" do
-        params = {"name" => "English 101", "teacher_id" => "1"}
-        Classroom.should_receive(:create).with(params).and_return(Classroom.new)
-        post :create, {:teacher_id => 1, :classroom => {name: "English 101", teacher_id: 1}}
+        params = {name: "English 101", teacher_id: 1}
+        post :create, {:teacher_id => 1, :classroom => params, format: :json}
+        json_response = JSON.parse(response.body).with_indifferent_access
+
+        expect(json_response[:name]).to eq("English 101")
+        expect(json_response[:teacher_id]).to eq(1)
+        expect(json_response[:created_at]).to be_nil
+        expect(json_response[:updated_at]).to be_nil
       end
     end
 
     context "with invalid attributes" do
       it "does not save the new classroom in the database without a name" do
-        params = {"teacher_id" => "1"}
-        Classroom.should_receive(:create).with(params).and_return(nil)
-        post :create, {:teacher_id => 1, :classroom => {teacher_id: 1}}
+        post :create, {:teacher_id => 1, :classroom => {teacher_id: 1}, format: :json}
+        json_response = JSON.parse(response.body).with_indifferent_access
+
+        expect(json_response[:errors]).to eq({"name"=>["can't be blank"]})
       end
 
       it "does not save the new classroom in the database without a teacher id" do
-        params = {"name" => "English 101"}
-        Classroom.should_receive(:create).with(params).and_return(nil)
-        post :create, {:teacher_id => 1, :classroom => {name: "English 101"}}
+        post :create, {:teacher_id => 1, :classroom => {name: "English 101"}, format: :json}
+        json_response = JSON.parse(response.body).with_indifferent_access
+        expect(json_response[:errors]).to eq({"teacher_id" => ["can't be blank"]})
       end
     end
   end
@@ -45,15 +52,21 @@ describe ClassroomsController do
 
     context "given valid attributes" do
       it "updates a classroom" do
-        @classroom = Classroom.create(teacher_id: 1, name: "Math 101")
-        params = {"name" => "English 101", "teacher_id" => "1"}
+        classroom = Classroom.create(teacher_id: 1, name: "Math 101")
+        classroom.name = "English 101"
+        params = {"name" => "English 101"}
+
         put :update, {
-                      :teacher_id => 1, :id => @classroom.id,
-                      :classroom => params
+                      :teacher_id => 1,
+                      :id => classroom.id,
+                      :classroom => params,
+                      format: :json
                      }
 
-        @classroom.reload
-        expect(@classroom.name).to eq "English 101"
+        # binding.pry
+        json_response = JSON.parse(response.body).with_indifferent_access
+
+        expect(json_response[:name]).to eq("English 101")
       end
     end
   end
